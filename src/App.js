823b3b79f9a48d7576b2a5dd8d17c1fa8aff7f45
Violet Stewart <vkazoo2@gmail.com>
2021-12-10 12:39:24 -0800

    filter function working with input

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { getCountries } from './services/Countries';
 
 function App() {
   const [Countries, setCountries] = useState([]);
+  const [query, setQuery] = useState('');
   useEffect(() => {
     const fetchData = async () => {
       const data = await getCountries();
@@ -12,10 +13,23 @@ function App() {
     };
     fetchData();
   }, []);
+
+  function filterCountries() {
+    const filteredCountries = Countries.filter((country) => country.name.includes(query));
+    return filteredCountries;
+  }
+
   return (
     <div className="App">
+      <input
+        className="input"
+        placeholder="search"
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
       <header>Countries of the World</header>
-      {Countries.map((r) => (
+      {filterCountries().map((r) => (
         <p key={r.id}>{r.name}</p>
       ))}
     </div>
