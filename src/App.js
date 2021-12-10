46043967890441e0f66777c77f37cc8c26120a2f
Violet Stewart <vkazoo2@gmail.com>
2021-12-10 15:07:48 -0800

    filter by continents

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { getCountries } from './services/Countries';
 function App() {
   const [Countries, setCountries] = useState([]);
   const [query, setQuery] = useState('');
+  const [Continent, setContinent] = useState('All');
+
   useEffect(() => {
     const fetchData = async () => {
       const data = await getCountries();
@@ -16,7 +18,12 @@ function App() {
 
   function filterCountries() {
     const filteredCountries = Countries.filter((country) => country.name.includes(query));
-    return filteredCountries;
+    console.log(filterCountries);
+    const filteredByContinents = filteredCountries.filter(
+      (country) => country.continent === Continent || Continent === 'All'
+    );
+    console.log(filteredByContinents);
+    return filteredByContinents;
   }
 
   return (
@@ -28,6 +35,17 @@ function App() {
         value={query}
         onChange={(e) => setQuery(e.target.value)}
       />
+      <select value={Continent} onChange={(e) => setContinent(e.target.value)}>
+        <option value="All">All</option>
+        <option value="Africa">Africa</option>
+        <option value="Antarctica">Antarctica</option>
+        <option value="Asia">Asia</option>
+        <option value="Oceania">Oceania</option>
+        <option value="Europe">Europe</option>
+        <option value="North America">Norh America</option>
+        <option value="South America">South America</option>
+      </select>
+
       <header>Countries of the World</header>
       {filterCountries().map((r) => (
         <p key={r.id}>{r.name}</p>
